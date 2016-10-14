ae054c2bb1005bd65c0104825fb22b91a76ab83e
Emil Stener <stener99@hotmail.se>
2016-10-14 14:37:46 +0200

    hej

--- nastlade-loopar/Program.cs
+++ nastlade-loopar/Program.cs
@@ -6,10 +6,100 @@ using System.Threading.Tasks;
 
 namespace nastlade_loopar
 {
+    //övning2 
+
     class Program
     {
         static void Main(string[] args)
         {
+            /*
+            for (int y = 0; y < 11; y++)
+            {
+
+                for (int x = 0; x < 11; x++)
+                {
+                    if (y % 2 == 0)
+                    {
+
+
+                        if (x % 2 == 0)
+                        {
+                            Console.Write("#");
+                        }
+                        else
+                        {
+                            Console.Write("o");
+                        }
+                    }
+
+                    else
+                    {
+                        if (x%2==0)
+                        {
+                            Console.Write("o");
+                        }
+
+                        else
+                        {
+                            Console.Write("#");
+                        }
+                    }
+
+
+                        {
+
+
+                        }
+                    }
+                Console.WriteLine();
+            }
+                
+            Console.ReadKey();
+            }
+        */
+
+
+
+
+
+
+            //Övning 3
+            /*
+            for (int i = 10; i > 0; i--)
+            {
+            for (int y = 0; y < i; y++)
+            {
+               Console.Write("#");
+            }
+            Console.WriteLine();
+            }
+            Console.ReadKey();
+
+                */
+
+
+            //övning4
+            for (int i = 0; i < 10; i++)
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    if (x < i)
+                    {
+                        Console.Write("-");
+                    }
+                    else
+                    {
+                        Console.Write("#");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
         }
-    }
+
 }
+}
+
+
+
